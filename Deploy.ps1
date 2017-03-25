a1f2da850d62f2f1f7b4dc14fbabaa5e51daa75d
EdLichtman <E.Lichtman2@gmail.com>
2017-03-24 22:56:17 -0400

    Make fix to backups storage

--- Deploy.ps1
+++ Deploy.ps1
@@ -16,26 +16,26 @@ Else {
 	}
 
 	###### Create New Directory for backup ######
-	$lastBackup = Get-ChildItem | Where-Object {$_.name -like "\backups\bin$(get-date -f yyyyMMdd)*"} | sort name -Descending | select -First 1
+	$lastBackup = Get-ChildItem backups | Where-Object {$_.name -like "bin$(get-date -f yyyyMMdd)*"} | sort name -Descending | select -First 1
 
 	If (!$lastBackup) {
-		$newBackupName = ".backups\bin$(get-date -f yyyyMMdd)"
+		$newBackupName = "backups\bin$(get-date -f yyyyMMdd)"
 	} Else {
 		$index = $lastBackup.Name.IndexOf("_")
 		If ($index -eq -1) {
-			$newBackupName = ".backups\bin$(get-date -f yyyyMMdd)_1"
+			$newBackupName = "backups\bin$(get-date -f yyyyMMdd)_1"
 		} Else {
 			$lastBackupVersion = $lastBackup.Name[$index+1]
-			$newBackupName = ".backups\bin$(get-date -f yyyyMMdd)_$($([convert]::ToInt32($lastBackupVersion, 10))+1)"
+			$newBackupName = "backups\bin$(get-date -f yyyyMMdd)_$($([convert]::ToInt32($lastBackupVersion, 10))+1)"
 		}
 	}
 	New-Item -ItemType directory -Path $newBackupName\bin
 
 	###### Copy the Bin and Web Config ######
-	Copy-Item .\bin\* .backups\$newBackupName\bin
+	Copy-Item .\bin\* .\$newBackupName\bin
 
 	If ((Get-ChildItem | Where-Object {$_.name -eq "Web.config"})){
-	Copy-Item .\Web.config .backups\$newBackupName\}
+	Copy-Item .\Web.config .\$newBackupName\}
 
 	###### Delete the contents of the bin ######
 	Remove-Item -path .\bin\* 
